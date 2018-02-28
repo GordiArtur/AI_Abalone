bbbe7899a51b2a44089438b22660ba35fc8ae3c3
tlin98 <tonylin514@gmail.com>
2018-02-28 02:28:51 -0800

    Added commenting for all methods and some private variables.

--- src/Board.java
+++ src/Board.java
@@ -3,12 +3,39 @@ import java.awt.*;
 
 public class Board extends JPanel {
 
+    /**
+     * Size of Hex display size for GUI
+     */
     private static final int HEX_SIZE = 90;
+
+    /**
+     * Size of board game.
+     */
     private static final int BOARD_SIZE = 9;
+
+    /**
+     * Array of Hex.
+     */
     private Hex[][] hexes;
+
+    /**
+     * Integer half the size of BOARD_SIZE;
+     */
     private static final int HALF_SIZE = 4;
+
+    /**
+     * Number of starting pieces per player.
+     */
     private static final int START_PIECE_COUNT = 14;
+
+    /**
+     * Number of white marbles.
+     */
     private int whiteCount;
+
+    /**
+     * Number of black marbles.
+     */
     private int blackCount;
 
     public Board() {
@@ -26,7 +53,9 @@ public class Board extends JPanel {
 
     }
 
-    // Draws the board in a hexagon shape
+    /**
+     * Generates the board hexes that stores an array of 9 x 9 hex.
+     */
     private void drawBoard() {
         for (int y = 0; y < BOARD_SIZE; y++) {
             int dx = (int) (Math.abs(HALF_SIZE - y) * ((double) HEX_SIZE / 2));
@@ -48,6 +77,12 @@ public class Board extends JPanel {
         }
     }
 
+    /**
+     * Return hex from board given x and y coordinates. Returns null if out-of-bounds.
+     * @param x X position
+     * @param y Y position
+     * @return Hex of specific X and Y position
+     */
     public Hex getHex(int x, int y) {
         try {
             if (x < 0 || x >= BOARD_SIZE || y < 0 || y >= BOARD_SIZE || hexes[y][x] == null) {
@@ -74,6 +109,14 @@ public class Board extends JPanel {
         return blackCount;
     }
 
+    /**
+     * Move piece from source hex to destination hex. If null space then throw away source hex.
+     * Redraws board.
+     * @param sx Source x position
+     * @param sy Source y position
+     * @param dx Destination x position
+     * @param dy Destination y position
+     */
     public void movePiece(int sx, int sy, int dx, int dy) {
         if (sx < 0 || sx >= BOARD_SIZE || sy < 0 || sy >= BOARD_SIZE || hexes[sy][sx] == null) { // Bad-Source
             return;
@@ -96,7 +139,9 @@ public class Board extends JPanel {
         }
     }
 
-    // Sets standard board configuration
+    /**
+     * Update board to display default layout.
+     */
     public void standardLayout() {
         for (int y = 0; y < 9; ++y) {
             if (Math.abs(HALF_SIZE - y) > 2) {
@@ -114,6 +159,9 @@ public class Board extends JPanel {
         hexes[6][6].setPiece(Color.BLACK);
     }
 
+    /**
+     * Update board to display german daisy layout.
+     */
     public void germanDaisy() {
         hexes[2][1].setPiece(Color.WHITE);
         hexes[2][5].setPiece(Color.BLACK);
@@ -133,6 +181,9 @@ public class Board extends JPanel {
         }
     }
 
+    /**
+     * Update board to display belgian daisy layout.
+     */
     public void belgianDaisy() {
         hexes[1][1].setPiece(Color.WHITE);
         hexes[1][4].setPiece(Color.BLACK);
@@ -152,6 +203,9 @@ public class Board extends JPanel {
         }
     }
 
+    /**
+     * All pieces on board is cleaned off and set to 0 marbles.
+     */
     public void clearBoard() {
         for (int i = 0; i < BOARD_SIZE; i++) {
             for (int j = 0; j < BOARD_SIZE; j++) {
@@ -164,7 +218,11 @@ public class Board extends JPanel {
     }
 
     /**
-     * 1 = Standard 2 = Belgian Daisy 3 = German Daisy
+     * Resets the board to one of 3 starting layouts.
+     * 1 = Standard
+     * 2 = Belgian Daisy
+     * 3 = German Daisy
+     * @param layout Integer value of board layout type
      */
     public void selectLayout(int layout) {
         clearBoard();
@@ -186,6 +244,7 @@ public class Board extends JPanel {
 
     /**
      * Returns the size of the board;
+     * @return Integer size of board
      */
     public int getBoardSize() {
         return BOARD_SIZE;
--- src/Human.java
+++ src/Human.java
@@ -13,11 +13,34 @@ import java.util.List;
  */
 public class Human extends JPanel implements Agent {
 
+    /**
+     * Array of hexes (3) that player has selected.
+     */
     private List<Hex> selectedHex;
+
+    /**
+     * The turn the player is on (1 or 0).
+     */
     private int activeTurn;
+
+    /**
+     * Reference to Board object from Game.
+     */
     private Board board;
+
+    /**
+     * Movement buttons.
+     */
     private JButton NE, E, SE, SW, W, NW;
+
+    /**
+     * Reference to Controls object from Game.
+     */
     private Controls humanControls;
+
+    /**
+     * Reference to Game object from Game
+     */
     private Game humanGame;
 
     public Human(Game game, Board board, Controls control) {
@@ -73,12 +96,21 @@ public class Human extends JPanel implements Agent {
         add(NE);
     }
 
+    /**
+     * Sets the game's current turn to this player's turn.
+     * @param board the game board
+     * @return the resulting game board.
+     */
     public Board play(Board board) {
         Game.turn = activeTurn;
         this.board = board;
         return this.board;
     }
 
+    /**
+     * Sorts the List<Hex> for selectedHex to arrange Hexes from origin point
+     * (top-left corner) in ascending order. Not generic code.
+     */
     public void sortSelected() {
         if (selectedHex.size() == 3) {
             List<Hex> unsorted = new ArrayList<Hex>(selectedHex);
@@ -102,8 +134,17 @@ public class Human extends JPanel implements Agent {
         }
     }
 
-    // Outputs in 1, 10, 11 (1,1 | 1,0 | 0,1) for direction
-    // Outputs 0 for error
+    /**
+     * Outputs an integer that represents the axial direction of the elements in selectedHex.
+     * 1 is vertical
+     * 10 is horizontal
+     * 11 is diagonal
+     * @param sx First hex x coordinate
+     * @param sy First hex y coordinate
+     * @param dx Last hex x coordinate
+     * @param dy Last hex y coordinate
+     * @return Integer Identity of axial direction
+     */
     private int identity(int sx, int sy, int dx, int dy) {
         int out = 0;
         if ((dx + dy) == (sx + sy)) {
@@ -116,6 +157,11 @@ public class Human extends JPanel implements Agent {
 
     class MovementListener implements ActionListener {
 
+        /**
+         * Checks which button is clicked, follow by executing validMove. Upon validMove
+         * is successful, updates Game's turn to AI.
+         * @param e ActionEvent from button
+         */
         @Override
         public void actionPerformed(ActionEvent e) {
             boolean played = false;
@@ -153,8 +199,19 @@ public class Human extends JPanel implements Agent {
             }
         }
 
-        /*
-         *
+        /**
+         * Checks if the selected hexes and called action is a legal move.
+         * 1. validMove sorts selectedHex array and reverses if moving away from origin
+         * 2. Generate special identity of direction
+         * 3. Generate identity of selectedHex array
+         * 4. First check identity to direction identity, Inline
+         * 4.1. If empty spaces or off-board area, move and return true
+         * 4.2. Else if no blocker pieces/equal number pieces, same color then move and return true
+         * 5 Second check for Broadside and single piece
+         * 5.1. Check for available space, move then return true
+         * return false for nullptr and blocked moves
+         * @param dx X value of horizontal movement
+         * @param dy Y value of vertical movement
          */
         private boolean validMove(int dx, int dy) { // Don't read it. It's very long
             sortSelected();
@@ -240,6 +297,9 @@ public class Human extends JPanel implements Agent {
 
     }
 
+    /**
+     * Sets background of selectedHex Hexes to default.
+     */
     private void clearSelected() {
         for (Hex h : selectedHex) {
             h.setDefaultColor();
@@ -247,6 +307,15 @@ public class Human extends JPanel implements Agent {
         selectedHex.clear();
     }
 
+    /**
+     * Checks if hex is admissible to selectedHex array.
+     * 1. If empty, add
+     * 2. else if duplicate, remove
+     * 3. else if not full
+     * 3.1. checks if hex's marble is the same as current player
+     * 3.2. checks if marbles are in a line
+     * @param hex
+     */
     private void addToSelection(Hex hex) {
         if (selectedHex.size() == 0) {
             selectedHex.add(hex);
@@ -293,6 +362,10 @@ public class Human extends JPanel implements Agent {
 
     class MouseListener extends MouseAdapter {
 
+        /**
+         * Checks if it's player's active turn before finding clicked Hex and adding to selectedHex.
+         * @param e Mouse Click event
+         */
         @Override
         public void mouseClicked(MouseEvent e) {
             if (Game.turn == activeTurn) {
