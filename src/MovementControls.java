833721e751270eb9e5b35e27582d9873e2832619
Artur <gordiartur@gmail.com>
2018-03-02 14:39:13 -0800

    Add keyboard shortcut listener Windows: alt + [q; a; z; c; d; e] Mac: ctrl + option + [q; a; z; c; d; e]

--- src/MovementControls.java
+++ src/MovementControls.java
@@ -1,9 +1,6 @@
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
+import java.awt.event.*;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -72,11 +69,23 @@ public class MovementControls extends JPanel {
         W = new JButton("West");
         NW = new JButton("North-West");
 
+        /**
+         * Add movement listener
+         * Add keyboard shortcut listener:
+         * Windows: alt + [q; a; z; c; d; e]
+         * Mac: ctrl + option + [q; a; z; c; d; e]
+         */
+        NW.setMnemonic(KeyEvent.VK_Q);
         NW.addActionListener(new MovementListener());
+        W.setMnemonic(KeyEvent.VK_A);
         W.addActionListener(new MovementListener());
+        SW.setMnemonic(KeyEvent.VK_Z);
         SW.addActionListener(new MovementListener());
+        SE.setMnemonic(KeyEvent.VK_C);
         SE.addActionListener(new MovementListener());
+        E.setMnemonic(KeyEvent.VK_D);
         E.addActionListener(new MovementListener());
+        NE.setMnemonic(KeyEvent.VK_E);
         NE.addActionListener(new MovementListener());
 
         add(NW);
